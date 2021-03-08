c8f64396edbd4dc2fa7f1f61b5103ad7d9c45035
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-08 20:15:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,6 @@ module "create_cluster_a" {
   dns_servers = var.dns_servers
   ntp_servers = var.ntp_servers
 
-  proxy_enabled = var.proxy_enabled
-
-  http_proxy_hostname = var.http_proxy_hostname
-  http_proxy_protocol = var.http_proxy_protocol
-  http_proxy_port = var.http_proxy_port
-  https_proxy_hostname = var.https_proxy_hostname
-  https_proxy_protocol = var.https_proxy_protocol
-  https_proxy_port = var.https_proxy_port
-
   vcenter_target = var.vcenter_target
   vcenter_cluster = var.vcenter_cluster
   vcenter_datastore = var.vcenter_datastore
